ce6e1a5b619d6a8df0378e4acc0644297f6b0494
Kathy Nguyen <kathyvng@gmail.com>
2018-06-20 05:02:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  pretty_stones.push = "quartz"
+  pretty_stones << "quartz"
 end
 
 def add_element_to_start_of_array(array, element)
